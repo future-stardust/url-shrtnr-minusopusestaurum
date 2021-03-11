8534dacf7c98d98ef7c5874bcbbbc681c5de6a93
Vladyslav <43961669+VladDishkant@users.noreply.github.com>
2021-03-11 13:45:19 +0200

    Update User.java

--- src/main/java/edu/kpi/testcourse/User.java
+++ src/main/java/edu/kpi/testcourse/User.java
@@ -1,56 +1,13 @@
 package edu.kpi.testcourse;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+
 /**
- * User class
- *
- * <p>Include all wich refers to user: id, email, password, token.
+ * User record type.
  */
-public class User {
-  private int id = 0;
-  private String email;
-  private String password;
-  private String token;
-
-  /**
-   * Constructor User class
-   *
-   * <p>Assigns id, email and password for User object.
-   */
-  public User(int id, String email, String password) {
-    this.id = id;
-    this.email = email;
-    this.password = password;
-  }
-
-  public int getId() {
-    return id;
-  }
-
-  public void setId(int id) {
-    this.id = id;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  public String getToken() {
-    return token;
-  }
-
-  public void setToken(String token) {
-    this.token = token;
-  }
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+public record User(String shortLink,
+                    String url,
+                    Integer userId,
+                    Integer usages) {
 }
